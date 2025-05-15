6a9653d49b0d3217e3407a9b1253a985ddb52ec5
fadhelbima <129093358+fadhelbima@users.noreply.github.com>
2025-05-15 15:33:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('.fa-bars');
 const navLink = document.querySelector('.nav-link');
 const times = document.querySelector('.fa-times');
+  import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js';
+
+      createChat({
+        webhookUrl: 'https://playground.naratel.net.id/webhook/bb279b80-4558-486f-9837-b6511edd129d/chat , '
+      });
+
 menu.addEventListener('click',function(){
     navLink.style.transform='translateX(-200px)';
     navLink.style.transition = 'all 1s';
